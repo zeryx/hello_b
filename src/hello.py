683d0678e9d970213cbc6044b46ff98ea5c1aed1
James Sutton <1892175+zeryx@users.noreply.github.com>
2020-01-14 12:43:00 -0400

    hey Jaime

--- src/hello.py
+++ src/hello.py
@@ -3,4 +3,4 @@ import Algorithmia
 # API calls will begin at the apply() method, with the request body passed as 'input'
 # For more details, see algorithmia.com/developers/algorithm-development/languages
 def apply(input):
-    return "hello {} from org B, we've pushed a new release. Hey Steph look at this!".format(input)
+    return "hello {} from org B, we've pushed a new release. Hey Jaime look at this!".format(input)
